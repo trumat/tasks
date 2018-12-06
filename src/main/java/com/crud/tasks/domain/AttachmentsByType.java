402d3df6c1d1e7bda13390b9e002f7dc5eb4246c
trumat <matru@protonmail.com>
2018-12-06 21:57:54 +0000

    Exercise 18.3

--- src/main/java/com/crud/tasks/domain/AttachmentsByType.java
+++ src/main/java/com/crud/tasks/domain/AttachmentsByType.java
@@ -1,10 +1,12 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
 public class AttachmentsByType{
+    @JsonProperty("trello")
     private Trello trello;
 }
--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -1,11 +1,14 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
 public class Trello {
+    @JsonProperty("board")
     private String board;
+    @JsonProperty("card")
     private String card;
 }
